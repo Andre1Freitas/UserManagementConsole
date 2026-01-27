fa763712c4998b4bf50b0bd67c12f7c3d687b562
Andre <andrefreitas09876@gmail.com>
2026-01-27 17:40:21 -0300

    feat: add GUID as a unique odentifier for Person entity

--- Entities/Pessoa.cs
+++ Entities/Pessoa.cs
@@ -1,13 +1,25 @@
-﻿namespace UserManagementConsole.Entities
+﻿using System.Text.Json.Serialization;
+
+namespace UserManagementConsole.Entities
 {
     public class Pessoa
     {
         public string Nome { get; set; }
         public int Idade { get; set; }
         public string Email { get; set; }
+        public Guid Id { get; private set; }
 
         public Pessoa(string nome, int idade, string email)
         {
+            Nome = nome;
+            Idade = idade;
+            Email = email;
+            Id = Guid.NewGuid();
+        }
+        [JsonConstructor]
+        public Pessoa(Guid id, string nome, int idade, string email)
+        {
+            Id = id;
             Nome = nome;
             Idade = idade;
             Email = email;
@@ -19,7 +31,9 @@
                 + " "
                 + Idade
                 + " "
-                + Email;
+                + Email
+                + " "
+                + Id;
         }
     }
 }
