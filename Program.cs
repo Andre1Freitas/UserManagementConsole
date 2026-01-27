7a8c654c23c54e2c2e4bb2b5f10ee731dc63abd8
Andre <andrefreitas09876@gmail.com>
2026-01-27 17:43:22 -0300

    feat: add edit functionality and migrate to JSON repository

--- Program.cs
+++ Program.cs
@@ -10,7 +10,7 @@ namespace UserManagementConsole
     {
         static void Main(string[] args)
         {
-            IUserRepository repository = new UserCsvRepository();
+            IUserRepository repository = new UserJsonRepository();
             GerenciadorPessoas gerenciador = new GerenciadorPessoas(repository);
 
             int opcao = 1;
@@ -21,6 +21,7 @@ namespace UserManagementConsole
                 Console.WriteLine("2 - Excluir pessoa da lista");
                 Console.WriteLine("3 - Mostrar lista inteira");
                 Console.WriteLine("4 - Procurar uma pessoa da lista");
+                Console.WriteLine("5 - Editar uma pessoa da lista");
                 Console.WriteLine("0 - Sair");
                 opcao = int.Parse(Console.ReadLine()!);
 
@@ -74,18 +75,18 @@ namespace UserManagementConsole
 
                         break;
                     case 2:
-                        Console.Write("Escreva o nome da pessoa a ser removida: ");
-                        nome = Console.ReadLine()!;
-                        gerenciador.RemoverPessoa(nome);
+                        Console.Write("Escreva o id da pessoa a ser removida: ");
+                        Guid id = Guid.Parse(Console.ReadLine()!);
+                        gerenciador.RemoverPessoa(id);
                         break;
                     case 3:
                         gerenciador.PercorrerLista();
                         Console.ReadLine();
                         break;
                     case 4:
-                        Console.Write("Escreva o nome da pessoa a ser procurada: ");
-                        nome = Console.ReadLine()!;
-                        Pessoa p = gerenciador.ProcuraUmaPessoaNaLista(nome);
+                        Console.Write("Escreva o id da pessoa a ser procurada: ");
+                        id = Guid.Parse(Console.ReadLine()!);
+                        Pessoa p = gerenciador.ProcuraUmaPessoaNaLista(id);
                         if (p == null)
                         {
                             Console.WriteLine("Não existe pessoa com esse nome");
@@ -95,6 +96,57 @@ namespace UserManagementConsole
                             Console.WriteLine(p);
                         }
                         Console.ReadLine();
+                        break;
+                    case 5:
+                        gerenciador.PercorrerLista();
+                        Console.WriteLine("Escreva as novas informações dessa pessoa:");
+
+                        Console.Write("Nome: ");
+                        nome = Console.ReadLine();
+
+                        resultNome = Validacoes.ValidarNome(nome);
+                        while (!resultNome.isValido)
+                        {
+                            Console.WriteLine(resultNome.mensagemErro);
+                            nome = Console.ReadLine();
+                            resultNome = Validacoes.ValidarNome(nome);
+                        }
+
+                        Console.Write("Idade: ");
+                        (bool isValido, string mensagemErro) resultIdad;
+                        while (true)
+                        {
+                            if (!int.TryParse(Console.ReadLine(), out idade))
+                            {
+                                Console.WriteLine("Idade Invalida. Digite um numero inteiro valido.");
+                                continue;
+                            }
+                            resultIdad = Validacoes.ValidarIdade(idade);
+                            if (!resultIdad.isValido)
+                            {
+                                Console.WriteLine(resultIdad.mensagemErro);
+                                continue;
+                            }
+                            break;
+                        }
+
+                        Console.Write("Email: ");
+                        email = Console.ReadLine()!;
+                        resultEmail = Validacoes.ValidarEmail(email);
+                        while (!resultEmail.isValido)
+                        {
+                            Console.WriteLine(resultEmail.mensagemErro);
+                            email = Console.ReadLine();
+                            resultEmail = Validacoes.ValidarEmail(email);
+                        }
+
+                        Console.Write("Id: ");
+                        Guid idEditar = Guid.Parse(Console.ReadLine());
+
+                        Pessoa pessoaEditada = new Pessoa(nome, idade, email);
+
+                        gerenciador.Edit(idEditar, pessoaEditada);
+
                         break;
                 }
 
