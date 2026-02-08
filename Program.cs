e8a662b3d40251cca604799c98bb76f1e8a67bcd
Andre <andrefreitas09876@gmail.com>
2026-02-08 08:03:09 -0300

    refactor: translate MenuConsole to English

--- Program.cs
+++ Program.cs
@@ -12,17 +12,17 @@ namespace UserManagementConsole
         {
             IUserRepository repository = new UserJsonRepository();
             PersonService personService = new PersonService(repository);
-            MenuConsole menu = new MenuConsole();
+            ConsoleMenu menu = new ConsoleMenu();
 
             int option = 1;
             while (option != 0)
             {
-                option = menu.ExibirMenuELerOpcao();
+                option = menu.DisplayMenuAndReadOption();
                 if (option < 0 || option > 5)
                 {
-                    menu.ExibirMensagem("Opção invalida!\n[Pressione Enter para continuar]", ConsoleColor.Red);
-                    menu.AguardarTecla();
-                    menu.LimparTela();
+                    menu.DisplayMessage("Opção invalida!\n[Pressione Enter para continuar]", ConsoleColor.Red);
+                    menu.WaitForKey();
+                    menu.ClearScreen();
                     continue;
                 }
                 switch (option)
@@ -30,59 +30,59 @@ namespace UserManagementConsole
                     case 0: break;
 
                     case 1:
-                        personService.AddUser(menu.ColetarDadosNovaPessoa());
-                        menu.ExibirMensagem("Pessoa cadastrada com sucesso!\n[Pressione Enter para continuar]", ConsoleColor.Green);
-                        menu.AguardarTecla();
+                        personService.AddUser(menu.CollectNewUserData());
+                        menu.DisplayMessage("Pessoa cadastrada com sucesso!\n[Pressione Enter para continuar]", ConsoleColor.Green);
+                        menu.WaitForKey();
                         break;
 
                     case 2:
                         List<User> users = personService.GetAll();
-                        if (menu.VerificarListaVazia(users, "Lista está vazia"))
+                        if (menu.IsListEmpty(users, "Lista está vazia"))
                         {
                             break;
                         }
-                        User userToDelete = menu.SelecionarPessoaDaLista(users);
-                        if (menu.ConfirmarAcao($"Tem certeza que deseja remover {userToDelete.Name}? (s/n):"))
+                        User userToDelete = menu.SelectUserFromList(users);
+                        if (menu.ConfirmAction($"Tem certeza que deseja remover {userToDelete.Name}? (s/n):"))
                         {
                             personService.DeleteUser(userToDelete.Id);
-                            menu.ExibirMensagem("Pessoa excluida com sucesso!\n[Pressione Enter para continuar]", ConsoleColor.Green);
-                            menu.AguardarTecla();
+                            menu.DisplayMessage("Pessoa excluida com sucesso!\n[Pressione Enter para continuar]", ConsoleColor.Green);
+                            menu.WaitForKey();
                             break;
                         }
-                        menu.ExibirMensagem("Pessoa não foi excluida!\n[Pressione Enter para continuar]", ConsoleColor.Red);
-                        menu.AguardarTecla();
+                        menu.DisplayMessage("Pessoa não foi excluida!\n[Pressione Enter para continuar]", ConsoleColor.Red);
+                        menu.WaitForKey();
                         break;
 
                     case 3:
-                        menu.ExibirLista(personService.GetAll());
-                        menu.AguardarTecla();
+                        menu.ShowList(personService.GetAll());
+                        menu.WaitForKey();
                         break;
 
                     case 4:
-                        if (menu.VerificarListaVazia(personService.GetAll(), "Lista está vazia"))
+                        if (menu.IsListEmpty(personService.GetAll(), "Lista está vazia"))
                         {
                             break;
                         }
-                        string nameSearch = menu.BuscarNomePessoa();
-                        menu.ExibirResultadosBusca(personService.SearchByName(nameSearch));
-                        menu.ExibirMensagem("[Pressione Enter para continuar]");
-                        menu.AguardarTecla();
+                        string nameSearch = menu.SearchUserName();
+                        menu.DisplaySearchResults(personService.SearchByName(nameSearch));
+                        menu.DisplayMessage("[Pressione Enter para continuar]");
+                        menu.WaitForKey();
                         break;
 
                     case 5:
                         users = personService.GetAll();
-                        if (menu.VerificarListaVazia(users, "Lista está vazia"))
+                        if (menu.IsListEmpty(users, "Lista está vazia"))
                         {
                             break;
                         }
-                        User existingUser = menu.SelecionarPessoaDaLista(users);
-                        User updatedUser = menu.ColetarDadosNovaPessoa();
+                        User existingUser = menu.SelectUserFromList(users);
+                        User updatedUser = menu.CollectNewUserData();
                         personService.Edit(existingUser.Id, updatedUser);
-                        menu.ExibirMensagem("Pessoa editada com sucesso!\n[Pressione Enter para continuar]", ConsoleColor.Green);
-                        menu.AguardarTecla();
+                        menu.DisplayMessage("Pessoa editada com sucesso!\n[Pressione Enter para continuar]", ConsoleColor.Green);
+                        menu.WaitForKey();
                         break;
                 }
-                menu.LimparTela();
+                menu.ClearScreen();
             }
         }
     }
---
+++ Utils/ConsoleMenu.cs
@@ -0,0 +1,197 @@
+﻿using UserManagementConsole.Entities;
+
+namespace UserManagementConsole.Utils
+{
+    public class ConsoleMenu
+    {
+        public int DisplayMenuAndReadOption()
+        {
+            Console.WriteLine("Selecione uma opção");
+            Console.WriteLine("1 - Cadastrar nova pessoa");
+            Console.WriteLine("2 - Excluir pessoa da lista");
+            Console.WriteLine("3 - Mostrar lista inteira");
+            Console.WriteLine("4 - Procurar uma pessoa da lista");
+            Console.WriteLine("5 - Editar uma pessoa da lista");
+            Console.WriteLine("0 - Sair");
+            if (int.TryParse(Console.ReadLine()!, out int option)) return option;
+            return -1;
+        }
+
+        public void ShowList(List<User> users)
+        {
+            if (users.Count == 0)
+            {
+                DisplayMessage("Nenhuma pessoa cadastrada", ConsoleColor.Red);
+                return;
+            }
+            for (int i = 0; i < users.Count; i++)
+            {
+                Console.WriteLine($"{i + 1}. {users[i].Name} ({users[i].Email})");
+            }
+        }
+        public void DisplaySearchResults(List<User> users)
+        {
+            if (users.Count == 0)
+            {
+                DisplayMessage("Nenhuma pessoa encontrada", ConsoleColor.Red);
+                return;
+            }
+            for (int i = 0; i < users.Count; i++)
+            {
+                Console.WriteLine($"{i + 1}. {users[i].Name} {users[i].Age} ({users[i].Email}) {users[i].Id}");
+            }
+        }
+
+        public void DisplayMessage(string message)
+        {
+            Console.WriteLine(message);
+        }
+        public void DisplayMessage(string message, ConsoleColor color)
+        {
+            Console.ForegroundColor = color;
+            Console.WriteLine(message);
+            Console.ResetColor();
+        }
+
+        public void ClearScreen()
+        {
+            Console.Clear();
+        }
+
+        public void WaitForKey()
+        {
+            Console.ReadLine();
+        }
+
+        public string RequestValidName()
+        {
+            Console.Write("Nome: ");
+            string name = Console.ReadLine();
+
+            var nameResult = Validacoes.ValidarNome(name);
+            while (!nameResult.isValido)
+            {
+                DisplayMessage($"{nameResult.mensagemErro}", ConsoleColor.Red);
+                name = Console.ReadLine();
+                nameResult = Validacoes.ValidarNome(name);
+            }
+            return name;
+        }
+
+        public int RequestValidAge()
+        {
+            Console.Write("Idade: ");
+            int age;
+            (bool isValido, string mensagemErro) ageResult;
+            while (true)
+            {
+                if (!int.TryParse(Console.ReadLine(), out age))
+                {
+                    DisplayMessage("Idade Invalida. Digite um numero inteiro valido.", ConsoleColor.Red);
+                    continue;
+                }
+                ageResult = Validacoes.ValidarIdade(age);
+                if (!ageResult.isValido)
+                {
+                    DisplayMessage($"{ageResult.mensagemErro}", ConsoleColor.Red);
+                    continue;
+                }
+                break;
+            }
+            return age;
+        }
+
+        public string RequestValidEmail()
+        {
+            Console.Write("Email: ");
+            string email = Console.ReadLine()!;
+            var emailResult = Validacoes.ValidarEmail(email);
+            while (!emailResult.isValido)
+            {
+                DisplayMessage($"{emailResult.mensagemErro}", ConsoleColor.Red);
+                email = Console.ReadLine();
+                emailResult = Validacoes.ValidarEmail(email);
+            }
+            return email;
+        }
+
+        public Guid RequestValidGuid(string msg)
+        {
+            while (true)
+            {
+                Console.Write(msg);
+                if (!Guid.TryParse(Console.ReadLine(), out Guid id))
+                {
+                    DisplayMessage("Guid invalido!", ConsoleColor.Red);
+                    continue;
+                }
+                return id;
+            }
+        }
+
+        public User CollectNewUserData()
+        {
+            Console.WriteLine("Escreva as informações dessa pessoa:");
+            return new User(RequestValidName(), RequestValidAge(), RequestValidEmail());
+        }
+
+        public User SelectUserFromList(List<User> users)
+        {
+
+            ShowList(users);
+            while (true)
+            {
+                Console.WriteLine("Escreva o numero da pessoa: ");
+                if (int.TryParse(Console.ReadLine(), out int num))
+                {
+                    if (num < 1 || num > users.Count)
+                    {
+                        DisplayMessage("Numero invalido! Tente novamente", ConsoleColor.Red);
+                        continue;
+                    }
+                    return users[num - 1];
+                }
+                DisplayMessage("Entrada invalida! Digite apenas numeros", ConsoleColor.Red);
+                continue;
+            }
+        }
+        public string SearchUserName()
+        {
+            while (true)
+            {
+                Console.WriteLine("Digite parte do nome da pessoa: ");
+                string? namePart = Console.ReadLine();
+                if (string.IsNullOrEmpty(namePart))
+                {
+                    DisplayMessage("Nome não pode ser vazio!", ConsoleColor.Red);
+                    continue;
+                }
+                return namePart!;
+            }
+        }
+        public bool ConfirmAction(string message)
+        {
+            while (true)
+            {
+                DisplayMessage(message, ConsoleColor.Yellow);
+                string? input = Console.ReadLine()?.ToLower();
+
+                if (input == "s") return true;
+                if (input == "n") return false;
+
+                DisplayMessage("Resposta invalida! Digite 's' para sim ou 'n' para não.", ConsoleColor.Red);
+            }
+        }
+        public bool IsListEmpty(List<User> users, string message)
+        {
+            if (users.Count == 0)
+            {
+                DisplayMessage(message, ConsoleColor.Red);
+                WaitForKey();
+                return true;
+            }
+            return false;
+        }
+    }
+
+}
